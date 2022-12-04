f321afe575bcaf158db29bc26bc4f995c5ac6be8
Joy <joywadhonkar4@gmail.com>
2022-12-04 20:00:21 +0530

    Create another routes and controller for users

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const express=require("express");
+const router=express.Router();
+const usersController=require("../controllers/users_controller");
+console.log("Router is loaded");
+
+router.get("/profile",usersController.profile);
+
+module.exports=router;
