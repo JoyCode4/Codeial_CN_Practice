d9112a902542a2a9c66ae9f78d990265e07d0a34
Joy <joywadhonkar4@gmail.com>
2023-01-22 13:57:41 +0530

    Create Sign up and Sign in pages and created route and controller for them

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ console.log("router loaded");
 router.get("/",homeController.home);
 router.use("/users",require("./users"));
 router.use("/media",require("./posts"));
+router.use("/",require("./sign_up"));
 
 // for any further routes, access from here
 // router.use("/routerName",require("./routerfile"));
