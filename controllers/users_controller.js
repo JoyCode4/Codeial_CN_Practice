a910059ff45e1194059893ded7924ad2debfa0bd
Joy <joywadhonkar4@gmail.com>
2023-01-28 14:47:54 +0530

    add logout link and session logout mechanism and also change home page css style normal and added routes and controllers for the logout mechanism

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -51,3 +51,11 @@ module.exports.create=function(req,res){
 module.exports.createSession=function(req,res){
     return res.redirect("/");
 }
+
+module.exports.destroySession = function(req,res){
+    // code is different from sir(callback function of req.logout)
+    req.logout((err)=>{
+        console.log("destroySession there is error");
+    });
+    return res.redirect("/");
+}
--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,6 @@ router.get("/sign_up",usersController.signUp);
 router.post("/create",usersController.create);
 // router.get("/post",postsController.post);
 router.post("/create-session",passport.authenticate("local",{failureRedirect:"/users/sign_in"}),usersController.createSession);
+
+router.get("/sign_out",usersController.destroySession);
 module.exports=router;
