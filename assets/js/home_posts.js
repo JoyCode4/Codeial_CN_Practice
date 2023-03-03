5e82b3561a6952af5d78b783964d0715ac5b3f00
JoyCode4 <joywadhonkar4@gmail.com>
2023-03-03 13:08:31 +0530

    the post creation function is totally done with ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -9,7 +9,8 @@
                 url : "/posts/posts-create",
                 data: newPostForm.serialize(),
                 success: function(data){
-                    console.log(data);
+                    let newPost = newPostDOM(data.data.post);
+                    $("#posts-list-container>ul").prepend(newPost);
                 },error:function(error){
                     console.log(error.responseText);
                 }
@@ -19,6 +20,33 @@
 
 
     // method to create a post in DOM
+    let newPostDOM = (post)=>{
+        return $(`<li id="post-${post._id}">
+        <small>
+            <a class="delete-post-button" href="/posts/destroy/?id=${post.id}">X</a>
+        </small>
+        ${post.content}
+        <br>
+        <span>
+            ${post.user.name}
+        </span>
+        <div class="post-comments">
+            <form id="" action="/comments/create-comments" method="post">
+                <span>Comments</span>
+                <br>
+                <input name="content" placeholder="Here place your Comments...." required>
+                <input type="hidden" name="post" value="${post._id}">
+                <input type="submit" value="Add Comment">
+                
+            </form>
+
+            <div class="post-comments-list">
+                <ul id="post-comments-${post._id}">
+                </ul>
+            </div>
+        </div>
+    </li>`)
+    }
     
     createPost();
 }
