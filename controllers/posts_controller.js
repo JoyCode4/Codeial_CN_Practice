9b281e3a7ab5c554d51516d6077b4578dd42977f
JoyCode4 <joywadhonkar4@gmail.com>
2023-03-03 12:50:22 +0530

    link the cdn file of jquery ajax and created the post create function through ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -24,10 +24,19 @@ module.exports.createPost=(req,res)=>{
 
 module.exports.createPost = async (req,res)=>{
     try{
-        await Post.create({
+        let post = await Post.create({
             content:req.body.content,
             user:req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post:post
+                },
+                message:"Post created!"
+            })
+        }
 
         req.flash("success","Post Published!")
         return res.redirect("back");
