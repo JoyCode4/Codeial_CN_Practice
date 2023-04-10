612b9429b497af48f3d486ea4aad8eb932e08f2f
JoyCode4 <joywadhonkar4@gmail.com>
2023-04-10 15:20:26 +0530

    create the destroy option in the api

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,51 +1,35 @@
 const Post = require("../../../models/post");
 const Comment = require("../../../models/comment");
 
-module.exports.index = async function(req,res){
-
-    let posts = await Post.find({})
+module.exports.index=async (req,res)=>{
+    let posts=await Post.find({})
+    .sort("-createAT")
     .populate("user")
     .populate({
-        path:"comments",
+        path : "comments",
         populate:{
             path:"user"
         }
     });
 
     return res.json(200,{
-        message:"List of posts",
+        message : "It is the V1 api Version of this application",
         posts:posts
     })
-    // return res.json(200,{
-    //     message:"List of posts",
-    //     posts:posts
-    // })
 }
 
-module.exports.destroy=async (req,res)=>{
+module.exports.destory=async (req,res)=>{
     try{
-        let post = await Post.findById(req.params.id);
-        // .id means converting the object id into string
-        // if(post.user == req.user.id){
-            post.remove();
-            await Comment.deleteMany({post:req.params.id});
-            // req.flash("success", "Post and associated comments deleted successfully!");
-            // return res.redirect("/");
-            return res.json(200,{
-                message:"Post and associated comments deleted successfully!"
-            })
-        // }
-        // else{
-        //     req.flash("error","You can't delete this post ..")
-        //     return res.redirect("/");
-        // }
-    }catch(err){
-        console.log(err);
-        // req.flash("error", err);
-        // return res.redirect("/");
-        return res.json(500,{
-            message:"Internal Server Error"
+        let post=await Post.findById(req.params.id);
+        post.remove();
+        await Comment.deleteMany({post:req.params.id});
+
+        return res.json(200,{
+            message:"Post {"+ post.content +"} is deleted successfully"
         })
-    }
-    
+    }catch(err){
+    return res.json(500,{
+        message : "Internal Server Error"
+    })
+}
 }
