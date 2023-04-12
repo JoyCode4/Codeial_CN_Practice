cd04a322d3f12eec4d69d3096826e53b2cf1ec98
JoyCode4 <joywadhonkar4@gmail.com>
2023-04-13 00:27:49 +0530

    It is finally run without error but the error is nothing the internet connetion is not proper

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,13 +3,13 @@ const ejs = require("ejs");
 const path = require("path");
 
 let transporter = nodemailer.createTransport({
-    service : "yahoo",
-    host : "smtp.yahoo.com",
+    service : "outlook",
+    host : "smtp.outlook.com",
     port:587,
     secure:false,
     auth:{
-        user:"jaywadhonkar4@yahoo.com",
-        pass:"joy00004@"
+        user:"jaywadhonkar@outlook.com",
+        pass:"joy00004"
     }
 });
 
