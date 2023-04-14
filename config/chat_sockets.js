af855872015337a08f60057c598772ffb4b8d4e8
JoyCode4 <joywadhonkar4@gmail.com>
2023-04-14 14:46:12 +0530

    this is the connection is made at the localhost/5000 route and the other connection group_join

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,22 @@
-module.exports.chatSockets = (socketServer)=>{
+
+module.exports.chatSockets = function(socketServer){
+    let io = require("socket.io")(socketServer);
+
     
+    io.sockets.on("connection",(socket)=>{
+        console.log("New Connection received "+socket.id);
+
+        socket.on("disconnect",function(){
+            console.log("Socket disconnected!");
+        })
+
+        socket.on("join_room",function(data){
+            console.log("joining request rec.",data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit("user_joined",data);
+        })
+    })
+
 }
